47804417cb803fa95960e61d71fbfe10932f04aa
Jennifer Dai <Jaydee@MacBook-Pro-10.local>
2017-02-26 16:32:16 -0500

    add method_detective.rb

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -1,22 +1,64 @@
 # Replace in the "<???>" with the appropriate method (and arguments, if any).
 # Uncomment the calls to catch these methods red-handed.
 
+
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+# "iNvEsTiGaTiOn".<static VALUE
+rb_str_swapcase(VALUE str)
+{
+    str = rb_str_dup(str);
+    rb_str_swapcase_bang(str);
+    return str;
+}>
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+# "zom".<               static VALUE
+rb_str_squeeze(int argc, VALUE *argv, VALUE str)
+{
+    str = rb_str_dup(str);
+    rb_str_squeeze_bang(argc, argv, str);
+    return str;>
 # => “zoom”
 
-# "enhance".<???>
+# "enhance".<               static VALUE
+rb_str_rjust(int argc, VALUE *argv, VALUE str)
+{
+    return rb_str_justify(argc, argv, str, 'r');
+}>
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+
+# "Stop! You’re under arrest!".<            static VALUE
+rb_str_upcase(VALUE str)
+{
+    str = rb_str_dup(str);
+    rb_str_upcase_bang(str);
+    return str;
+}>
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+
+# "the usual".<               static VALUE
+rb_str_slice_bang(int argc, VALUE *argv, VALUE str)
+{
+    VALUE result;
+    VALUE buf[3];
+    int i;
+
+    rb_check_arity(argc, 1, 2);
+    for (i=0; i<argc; i++) {
+        buf[i] = argv[i];
+    }
+    str_modify_keep_cr(str);
+    result = rb_str_aref_m(argc, buf, str);
+    if (!NIL_P(result)) {
+        buf[i] = rb_str_new(0,0);
+        rb_str_aset_m(argc+1, buf, str);
+    }
+    return result;
+}>
 #=> "the usual suspects"
 
 # " suspects".<???>
@@ -28,10 +70,21 @@
 # "The mystery of the missing first letter".<???>
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+# "Elementary,    my   dear        Watson!".<static VALUE
+rb_str_rjust(int argc, VALUE *argv, VALUE str)
+{
+    return rb_str_justify(argc, argv, str, 'r');
+}>>
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+# "z".< VALUE
+rb_str_ord(VALUE s)
+{
+    unsigned int c;
+
+    c = rb_enc_codepoint(RSTRING_PTR(s), RSTRING_END(s), STR_ENC_GET(s));
+    return UINT2NUM(c);
+}
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
